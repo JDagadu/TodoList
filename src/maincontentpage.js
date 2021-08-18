5f80aa96018a92305588889b96a6e31c92b425f2
JDagadu <jeffdagadu@gmail.com>
2021-08-18 20:39:47 +0000

    header, sidebar and maincontentpage

--- src/maincontentpage.js
+++ src/maincontentpage.js
@@ -1,7 +1,7 @@
 import {elt} from '../../restaurantpage/src/domelementbuilder';
 
 function buildMainContentPage(){
-    return elt('div',{className:'h-screen w-full ml-72  content-main',id:'content-main'},'Hello, world!');
+    return elt('div',{className:'h-screen w-full ml-72  content-main',id:'content-main'},'Lovely Night Seygsyyyyy! Did not mean to give you a headache Dear.');
 
 
 }
